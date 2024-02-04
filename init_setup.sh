0cb8b5f298da48e99896532012e2be316d892e51
lintosunny <lintosunny111@gmail.com>
2024-02-04 15:14:18 +0530

    Added shell scipt to automate env setup

---
+++ init_setup.sh
@@ -0,0 +1,13 @@
+# script to automate env creation 
+
+echo [$(date)]: "START"
+echo [$(date)]: "creating env with python 3.8 version"
+conda create --prefix ./env python=3.8 -y
+
+echo [$(date)]: "activating the env"
+source activate ./env
+
+echo [$(date)]: "installing the dev requirements"
+pip install -r requirements.txt
+
+echo [$(date)]: "END"
