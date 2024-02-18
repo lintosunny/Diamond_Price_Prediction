0fe5148aa88cc13f1ac85c7b39325183b2b8c7e2
lintosunny <lintosunny111@gmail.com>
2024-02-18 16:09:18 +0530

    Added app.py

--- init_setup.sh
+++ init_setup.sh
@@ -5,7 +5,7 @@ echo [$(date)]: "creating env with python 3.8 version"
 conda create --prefix ./env python=3.8 -y
 
 echo [$(date)]: "activating the env"
-source activate ./env
+source activate .env
 
 echo [$(date)]: "installing the dev requirements"
 pip install -r requirements.txt
